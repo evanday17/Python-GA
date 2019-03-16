e1b880ac778cbb3d2b04c217cb5547897ed52b1a
EDay <evanday17@gmail.com>
2019-03-16 14:05:22 -0400

    basic prints

---
+++ comments.py
@@ -0,0 +1,6 @@
+#test
+
+print("Check time...if time is < than 5PM...keep working")
+print('if => 5pm being to leave the building')
+print('get up from desk and walk to the elavator')
+print('get in elavator and go to ground floor')
