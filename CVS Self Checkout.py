f141ee40a1bf1bb91ee821f44f100f008cef7f99
EDay <evanday17@gmail.com>
2019-04-01 09:52:15 -0400

    just need to figure out how to remove the price from the price list

--- CVS Self Checkout.py
+++ CVS Self Checkout.py
@@ -1,3 +1,6 @@
+# this is a "self-checkout" app
+# this isn't perfect and can be broken
+# for 'R' i figured out how to remove the item from the list but i need to figure out how to remove the price as well
 product = []
 price = []
 discount_amount = []
@@ -14,7 +17,7 @@ while True:
 
     enter_choice = input('***Please select a choice***') # get user choice
 
-    if enter_choice == 'P':
+    if enter_choice == 'P': # allow user to enter product and price
         enter_product = input("what product are you purchasing?")
         enter_price = int(input("how much is it?"))
         product.append(enter_product)
@@ -25,31 +28,32 @@ while True:
 
     elif enter_choice == 'D':
         enter_discount = int(input("please enter discount amount"))
-        discount_amount.append(enter_discount)
+        discount_amount.append(enter_discount) # add discount to discount list
 
     elif enter_choice == 'T':
         print('***Your total so far is ',(sum(price)),'***')
         print('Please note: This is not including sales tax')
 
     elif enter_choice == 'L':
-        print(product)
+        print('so far you have selected the following...')
+        print(product) # print prdocut list to show user what they have already scanned
 
     elif enter_choice == 'F':
-        total = (sum(price) - sum(discount_amount))
-        final_total = (total * .07) + total
+        total = (sum(price) - sum(discount_amount)) # take total and subtract discount
+        final_total = (total * .07) + total # apply sales tax
         print('***After applying a discount of ', discount_amount ,' your final total is ', final_total, '***')
         print('You purchased the following...')
         print(product, price)
         exit()
 
-    elif enter_choice == "R":
+    elif enter_choice == "R": # need to figure out how to remove the price as well as the product
         print('So far you have selected the following...')
         print(product)
         remove_item = input("Which would you like to remove")
         if remove_item in product:
             product.remove(remove_item)
             print('Item Removed...Here is your new list:')
-            print(product) # need to remove the price fromt he list as well as the item
+            print(product)
         else:
             print("Ok, got it")
 
