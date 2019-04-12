36f5cc6823f75105ffd1367ebe21aa3b4b709807
EDay <evanday17@gmail.com>
2019-04-12 13:37:07 -0400

    have my loops working need to build functions still

--- ATM v2.py
+++ ATM v2.py
@@ -1,85 +1,102 @@
-def main():
-    class Bank:
-        data_base = {  # create the dict of user names, pws and balances
+#  the class is what is inside my bank and how to complete a transaction
+#  the object is the transaction ?????????????????????????????
 
-        "John_123":
-        {
-            'Password': 'PW_1000',
-            'Checking Balance': 5.00,
-            'Savings Balance': 75.89,
+
+class Bank:
+        def __init__(self,):
+
+            self.data_base = {  # create the dict of user names, pws and balances
+
+                "John_123":
+                {
+                    'Password': 'PW_1000',
+                    'Checking Balance': 5.00,
+                    'Savings Balance': 75.89,
+                    },
+
+                'Joe_456':
+                {
+                    'Password': 'PW_1001',
+                    'Checking Balance': 500.00,
+                    'Savings Balance': 1050.89,
                 },
 
-        'Joe_456':
-        {
-            'Password': 'PW_1001',
-            'Checking Balance': 500.00,
-            'Savings Balance': 1050.89,
-            },
+                'Billy_789':
+                {
+                    'Password': 'PW_1002',
+                    'Checking Balance': 760.00,
+                    'Savings Balance': 3450.89,
+                    },
 
-        'Billy_789':
-        {
-            'Password': 'PW_1002',
-            'Checking Balance': 760.00,
-            'Savings Balance': 3450.89,
-                },
+                'Nick_000':
+                {
+                    'Password': 'PW_1003',
+                    'Checking Balance': 5000.00,
+                    'Savings Balance': 10500.89
+                }
 
-        'Nick_000':
-        {
-            'Password': 'PW_1003',
-            'Checking Balance': 5000.00,
-            'Savings Balance': 10500.89
             }
+            print("Hello, Welcome to the ATM")
+            self.user_validation = input("Please enter your username")
+            self.password_validation = input('Please enter your password')
 
-        }
+            def savings_balance(self):
+                for keys, values in self.data_base.items():
+                    for k, v in values.items():
+                        if keys == self.user_validation:
+                            if k == 'Savings Balance':
+                                print(v)
 
-        print("Hello, Welcome to the ATM")
-        user_validation = input("Please enter your username")
-        password_validation = input('Please enter your password')
+                def checkings_balance(self):  # print the checkings balance
+                    pass
 
-        def savings_balance(self):
-            for keys, values in data_base.items():
-                for k, v in values.items():
-                    if keys == user_validation:
-                        if k == 'Savings Balance':
-                            print(v)
+                def deposit(self):  # add deopsit to either the checkings or savings account
+                    pass
+
+                def withdraw(
+                        self):  # take user input and subtract that from either the savings account or checkings accont
+                    print("How much would you like to withdraw?")
+
+            while True:
+                if self.user_validation in self.data_base:  # checks to see that the user name in in the dict
+                    for values in self.data_base.values():  # iterates through the values
+                        if values['Password'] == self.password_validation:  # if the value of pw is == user input...start program
+                            print('Great, welcome ', self.user_validation)
+                            print('How can I help you today?')
+                            print()
+                            print()
+                            print('To check your savings balance: Press S')
+                            print('To check your checkings balance: Press C')
+                            print('To deposit money: Press D')
+                            print('To withdrawal money: Press W')
+                            print('To exit: Press E')
+                else:
+                    print("You entered an invalid user name and/or password")
+
+                self.user_input = input('Please select an option')
+
+                if self.user_input == 'C':
+                    checkings_balance()
+
+                elif self.user_input == 'S':
+                    savings_balance()
+
+                elif self.user_input == 'D':
+                    deposit()
+
+                elif self.user_input == 'W':
+                    withdraw()
+
+                elif self.user_input == 'E':
+                    exit()
+
+                else:
+                    print('You enter an invalid selection')
 
 
-        def checkings_balance(self):  # print the checkings balance
-            pass
+def main():
+    chase = Bank()
 
-        def deposit(self):  # add deopsit to either the checkings or savings account
-            pass
-
-        def withdraw(self):  # take user input and subtract that from either the savings account or checkings accont
-            print("How much would you like to withdraw?")
-
-        if user_validation in data_base:  # checks to see that the user name in in the dict
-            for values in data_base.values():  # iterates through the values
-                if values['Password'] == password_validation:  # if the value of pw is == user input...start program
-                    print('Great, welcome ', user_validation)
-                    print('How can I help you today?')
-                    print()
-                    print()
-                    print('To check your savings balance: Press S')
-                    print('To check your checkings balance: Press C')
-                    print('To deposit money: Press D')
-                    print('To withdrawal money: Press W')
-                    print('To exit: Press E')
-        else:
-            print("You entered an invalid user name and/or password")
-        user_input = input('Please select an option')
-        if user_input == 'C':
-            checkings_balance()
-        elif user_input == 'S':
-            savings_balance()
-        elif user_input == 'D':
-            deposit()
-        elif user_input == 'W':
-            withdraw()
-        elif user_input == 'E':
-            exit()
-        else:
-            print('You enter an invalid selection')
 
 
 if __name__ == '__main__':
--- class cake.py
+++ class cake.py
@@ -38,3 +38,20 @@ if user_name in data_base:
             print('this worked')
 else:
     print('no')
+
+
+def savings_balance(self):
+    for keys, values in self.data_base.items():
+        for k, v in values.items():
+            if keys == self.user_validation:
+                if k == 'Savings Balance':
+                    print(v)
+
+    def checkings_balance(self):  # print the checkings balance
+        pass
+
+    def deposit(self):  # add deopsit to either the checkings or savings account
+        pass
+
+    def withdraw(self):  # take user input and subtract that from either the savings account or checkings accont
+        print("How much would you like to withdraw?")
